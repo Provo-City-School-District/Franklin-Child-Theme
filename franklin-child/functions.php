64aa6f8a82b5219ba3b91180920705a6cc362c8a
Espi <joshespi@gmail.com>
2022-01-20 14:09:08 -0700

    added favicon

--- franklin-child/functions.php
+++ franklin-child/functions.php
@@ -8,7 +8,18 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', '' , '1.6', false);
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
+/*==========================================================================================
+// Favicon
+============================================================================================*/
+function pcsd_add_favicon(){ ?>
+	<!-- Custom Favicons -->
+	<link rel="shortcut icon" href="<?php echo get_theme_file_uri(); ?>/assets/images/favicon.png"/>
+	<link rel="apple-touch-icon" href="<?php echo get_theme_file_uri(); ?>/assets/images/favicon.png">
+	<?php }
+//add the favicon link to the live site head
+add_action('wp_head','pcsd_add_favicon');
+//add the favicon to the login page
+add_action('login_head','pcsd_add_favicon');
 /*==========================================================================================
 Custom Excerpt
 ============================================================================================*/
